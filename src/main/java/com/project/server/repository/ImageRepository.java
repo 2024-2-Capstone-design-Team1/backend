8b512beda403c4e15de61f0295268ed2259c5e22
minyeong <my011208@gmail.com>
2024-10-14 14:07:49 +0900

    초기환경 설정 및 테스트

--- src/main/java/com/project/server/repository/ImageRepository.java
+++ src/main/java/com/project/server/repository/ImageRepository.java
@@ -1,6 +1,7 @@
 package com.project.server.repository;
 
+import com.project.server.domain.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ImageRepository {
+public interface ImageRepository extends JpaRepository<Image, String>{
 }
---
+++ src/main/java/com/project/server/service/ImageService.java
@@ -0,0 +1,8 @@
+package com.project.server.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageService {
+
+}
