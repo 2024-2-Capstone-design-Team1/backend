3f8e01521e54621c249e2803a435c32de9fbe05f
minyeong <my011208@gmail.com>
2024-11-09 18:41:05 +0900

    dd

--- src/main/java/com/project/server/repository/MedicineRepository.java
+++ src/main/java/com/project/server/repository/MedicineRepository.java
@@ -1,9 +1,9 @@
-//package com.project.server.repository;
-//
-//import com.project.server.domain.Medicine;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface MedicineRepository extends JpaRepository<Medicine, Long> {
-//
-//    Medicine findByName(String name);
-//}
+package com.project.server.repository;
+
+import com.project.server.domain.Medicine;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicineRepository extends JpaRepository<Medicine, Long> {
+
+    Medicine findByName(String name);
+}
--- src/main/java/com/project/server/service/MedicineService.java
+++ src/main/java/com/project/server/service/MedicineService.java
@@ -1,41 +1,41 @@
-//package com.project.server.service;
-//
-//import com.project.server.domain.Medicine;
-//import com.project.server.repository.MedicineRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//@RequiredArgsConstructor
-//@Service
-//public class MedicineService {
-//    private final MedicineRepository medicineRepository;
-//
-//    //상비약 등록
-//    public Medicine save(Medicine medicine){
-//        return medicineRepository.save(medicine);
-//    }
-//
-//    //상비약 조회
-//    public Medicine getMedicineByName(String name){
-//        return medicineRepository.findByName(name);
-//    }
-//
-//    //삭제
-//    public boolean deleteMedicine(Long id){
-//        Medicine existingMedicine= medicineRepository.findById(id).orElse(null);
-//        if (existingMedicine != null){
-//            medicineRepository.delete(existingMedicine);
-//            return true;
-//        }
-//        return false;
-//    }
-//
-//    //추출한 텍스트 저장
-//    public Medicine saveText(String name, String description) {
-//        Medicine medicine = Medicine.builder()
-//                .name(name)
-//                .description(description)
-//                .build();
-//        return medicineRepository.save(medicine);
-//    }
-//}
+package com.project.server.service;
+
+import com.project.server.domain.Medicine;
+import com.project.server.repository.MedicineRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MedicineService {
+    private final MedicineRepository medicineRepository;
+
+    //상비약 등록
+    public Medicine save(Medicine medicine){
+        return medicineRepository.save(medicine);
+    }
+
+    //상비약 조회
+    public Medicine getMedicineByName(String name){
+        return medicineRepository.findByName(name);
+    }
+
+    //삭제
+    public boolean deleteMedicine(Long id){
+        Medicine existingMedicine= medicineRepository.findById(id).orElse(null);
+        if (existingMedicine != null){
+            medicineRepository.delete(existingMedicine);
+            return true;
+        }
+        return false;
+    }
+
+    //추출한 텍스트 저장
+    public Medicine saveText(String name, String description) {
+        Medicine medicine = Medicine.builder()
+                .name(name)
+                .description(description)
+                .build();
+        return medicineRepository.save(medicine);
+    }
+}
